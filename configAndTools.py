da498fab69f690cf491fe9f6d651f6eb6c935f77
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2025-11-14 23:08:34 -0500

    work on renderer and base sprite

--- configAndTools.py
+++ configAndTools.py
@@ -32,8 +32,78 @@ class GameLogic:
     def start(self) -> None:
         pass
 
+
+
+class BasicSprite(pygame.sprite.Sprite):
+    def __init__(self,x,y,image:pygame.surface.Surface,cameraX,cameraY) -> None:
+        super().__init__()
+        #init texture
+        self.currentTexture:pygame.surface.Surface=image
+        self.image:pygame.surface.Surface=self.currentTexture
+        self.rect:pygame.rect.Rect=self.image.get_rect()
+        #init pos and visibility
+        self.visible=True
+        self.x=x
+        self.y=y
+        self.cameraX=cameraX
+        self.cameraY=cameraY
+        #init image pos
+        self.rect.x=self.x-self.cameraX
+        self.rect.y=self.y-self.cameraY
+
+    def hide(self):
+        if(self.visible):
+            self.currentTexture=self.image
+            self.image=pygame.surface.Surface((self.rect.width,self.rect.height))
+
+    def show(self):
+        if(not self.visible):
+            self.image=self.currentTexture
+
+    def changeTexture(self, newTexture:pygame.surface.Surface):
+        self.currentTexture=newTexture
+        if(self.visible):
+            self.image=newTexture
+        self.rect=self.currentTexture.get_rect()
+        self.rect.x=self.x-self.cameraX
+        self.rect.y=self.y-self.cameraY
+
+    def setPos(self,x,y):
+        self.rect.x=x-self.cameraX
+        self.rect.y=y-self.cameraY
+
+    def move(self,x,y):
+        self.rect.x
+
+    def update(self, operation:int,argumentTuple:tuple) -> None:
+        if(operation==0):
+            self.cameraX=argumentTuple[0]
+            self.cameraY=argumentTuple[1]
+            self.rect.x=self.x-self.cameraX
+            self.rect.y=self.y-self.cameraY
+            
+
+
+
+
+
+class Camera:
+    def __init__(self,x,y) -> None:
+        self.x=x
+        self.y=y
+
+    def getPos(self):
+        return (self.x, self.y)
+    
+    def setPos(self,x,y):
+        self.x=x
+        self.y=y
+
+    
+
+
 class Renderer:
-    def __init__(self,displayWidth, displayHeight, clearColor) -> None:
+    def __init__(self,displayWidth:int, displayHeight:int, clearColor:tuple,layers:int) -> None:
         #config stuff
         self.displayWidth=displayWidth
         self.displayHeight=displayHeight
@@ -44,6 +114,15 @@ class Renderer:
         #flags for controlling what gets rendered and when
         self.frameChanged=True
         self.lastSize=(0,0)
+
+        #sprite layer stuff, because everything is a sprite
+        self.layerCount:int=layers
+        self.layers:list[pygame.sprite.Group]=[pygame.sprite.Group() for l in range(layers)]
+
+        self.camera=Camera(0,0)
+
+        #placeholder
+        self.uiContainer=None
         
 
 
@@ -52,16 +131,29 @@ class Renderer:
         screenSize=self.screen.get_size()
         if(self.frameChanged or (self.lastSize!=screenSize)):
             self.lastSize=screenSize
+            #put render menu code here
             pygame.transform.smoothscale(self.frameBuffer,screenSize,self.screen)
             pygame.display.flip()
+            self.frameChanged=False
 
  
 
     def render(self) -> None:
         self.frameChanged=True
-        pass
-        
-        
+        for layer in self.layers:
+            #put camera code here
+            layer.draw(self.frameBuffer)
+
+    def getCurrentCamera(self):
+        return self.camera
+    
+
+    def setCurrentCamera(self,camera:Camera):
+        self.camera=camera
+
+    def moveCamera(self,x,y):
+        self.camera
+
 
     def start(self) -> None:
         self.screen=pygame.display.set_mode(size=(self.displayWidth, self.displayHeight),vsync=1,flags=pygame.SCALED|pygame.RESIZABLE)
